5e4f80e688803bee0ce16e1535a4e639f7921e96
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-02-06 15:41:18 +0530

    adding more dto rules

--- src/dto/user.dto.js
+++ src/dto/user.dto.js
@@ -1,14 +1,19 @@
-import {z} from "zod";
+import { optional, z } from "zod";
 
 export const createUserDTO = z.object({
-    name : z.string().min(3,"name must be at least 3 chars"),
-    email: z.string().email("invalid email"),
-
+  name: z.string().min(3, "name must be at least 3 chars"),
+  email: z.string().email("invalid email"),
+  isActive: z.boolean({
+    required_error: "isActive is required",
+    invalid_type_error: "isActive must be true or false",
+  }),
+  age: z.number().int().min(18,"age must be atleast 18")
+  .max(60,"age must be atleast or less then 60"),
 });
 
-
 export const updateUserDTO = z.object({
-    name: z.string().min(3).optional(),
-    email: z.string().email().optional(),
-
+  name: z.string().min(3).optional(),
+  email: z.string().email().optional(),
+  isActive: z.boolean().optional(),
+   age: z.number().int().min(18).max(60),
 });
