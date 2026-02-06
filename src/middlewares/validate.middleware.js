99bba8b2acbfedc1fb8fe3b33a3d8d8e33ef5fa8
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-02-06 12:53:25 +0530

    polishing

--- src/middlewares/validate.middleware.js
+++ src/middlewares/validate.middleware.js
@@ -1,26 +1,24 @@
 import { ZodError } from "zod";
 
-export const validate = (schema) => (req, res, next) =>{
-    try{
-        req.body = schema.parse(req.body);
-        next();
-    }
-    catch (err){
-        console.log("ZOD ERROR: ",err); //keep this once
+export const validate = (schema) => (req, res, next) => {
+  try {
+    req.body = schema.parse(req.body);
+    next();
+  } catch (err) {
+    console.log("ZOD ERROR: ", err); //keep this once
 
-        if (err instanceof ZodError){
-            return res.status(400).json({
-                message: "validation failed",
-                errors: err.issues, // this is the key
-            });
-        }
-
-        return res.status(500).json({
-            message:"Internal server error",
-        });
+    if (err instanceof ZodError) {
+      return res.status(400).json({
+        message: "validation failed",
+        errors: err.issues, // this is the key
+      });
     }
+
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
 };
-
 
 //this is a custom validation middleware that uses a zod schema
 // to validate the incomming request body.
